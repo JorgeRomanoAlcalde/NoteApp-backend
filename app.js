10e1b6aff103f173c5f14f9c7293253d150e56e8
PalakMersus <98815347+palakMersus@users.noreply.github.com>
2025-05-02 15:13:20 +0100

    [Fix] Connection with frontend [Add] gitignore & package json

--- app.js
+++ app.js
@@ -3,43 +3,42 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 // express app
 const app = express();
 
+// CORS setup
 const corsOptions = {
   origin: 'http://localhost:4200',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 
-app.use(bodyParser.raw());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// connect to mongodb & listen for requests
-const dbURI = 'mongodb+srv://jorge:test1234@node.jnznlo6.mongodb.net/node?retryWrites=true&w=majority&appName=Node';
-
-mongoose.connect(dbURI)
-  .then(result => app.listen(3000))
-  .catch(err => console.log(err));
-
-// register view engine
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
-// middleware & static files
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+// Custom middleware to expose the path
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
 
-// routes
+// MongoDB connection
+const dbURI = 'mongodb+srv://jorge:test1234@node.jnznlo6.mongodb.net/node?retryWrites=true&w=majority&appName=Node';
+
+mongoose.connect(dbURI)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running on port 3000 and MongoDB connected');
+    });
+  })
+  .catch(err => console.log(err));
+
+// Routes
 app.get('/', (req, res) => {
   res.redirect('/blogs');
 });
@@ -48,73 +47,50 @@ app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-// blog routes
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
 
-app.get('/blogs',cors(corsOptions), (req, res) => {
+app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
     .then(result => {
-      res.render('index', { blogs: result, title: 'All blogs' });
-      console.log(req);
+      res.status(200).json({ blogs: result, title: 'All blogs' })
+      //res.render('index', { blogs: result, title: 'All blogs' });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 });
 
 app.post('/blogs', (req, res) => {
-  // console.log(req.body);
   const blog = new Blog(req.body);
 
   blog.save()
-    .then(result => {
+    .then(() => {
       res.redirect('/blogs');
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 });
 
 app.get('/blogs/:id', (req, res) => {
   const id = req.params.id;
+
   Blog.findById(id)
     .then(result => {
       res.render('details', { blog: result, title: 'Blog Details' });
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 });
 
 app.delete('/blogs/:id', (req, res) => {
   const id = req.params.id;
-  
+
   Blog.findByIdAndDelete(id)
-    .then(result => {
+    .then(() => {
       res.json({ redirect: '/blogs' });
     })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-app.delete('/blogs/:id',(req,res)=>{
-  const id = req.params.id;
-
-  Blog.findByIdAndDelete(id)
-  .then(results=>{
-    res.json({ redirect: '/blogs'})
-  })
-  .catch((err)=>{
-    console.log(err);
-  });
+    .catch(err => console.log(err));
 });
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
-
-app.listen(1000);
