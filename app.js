eccb7fa4b858b8bb31683b5662ec921696c295f4
JORGE <jorgealmacen25@gmail.com>
2025-05-07 17:04:49 +0100

    [Add] discard updates and delete created for notes and folders

--- app.js
+++ app.js
@@ -41,38 +41,140 @@ mongoose.connect(dbURI)
   .catch(err => console.log(err));
 
 
-  // Routes
-app.get('/', (req, res) => {
-  res.redirect('/blogs');
-});
-
-app.get('/blogs', (req, res) => {
+// Return notes all info
+app.get('/notes', (req, res) => {
+  const allNotesArr = [];
   const pinnedNotesArr = [];
   const otherNotesArr = [];
+  const discardNotesArr = [];
+  const folderArr = []
 
-  Note.find().sort({ createdAt: -1 })
-    .then(result => {
-      result.forEach(note => {
-        if (note.pinned === true) {
-          pinnedNotesArr.push(note);
-        } else {
-          otherNotesArr.push(note);
-        }        
+  const getFolders = Folder.find().sort({ createdAt: -1 }).exec();
+  const getNotes = Note.find().sort({ createdAt: -1 }).exec();
+
+  Promise.all([getFolders, getNotes])
+    .then(([foldersResult, notesResult]) => {
+      foldersResult.forEach(folder => {
+        folderArr.push(folder);
       });
-      res.status(200).json({ pinnedNotes: pinnedNotesArr, otherNotes: otherNotesArr});
+
+      notesResult.forEach(note => {
+        if (note.discard === true) {
+          discardNotesArr.push(note);
+        } else {
+          allNotesArr.push(note);
+          if (note.pinned === true) {
+            pinnedNotesArr.push(note);
+          } else {
+            otherNotesArr.push(note);
+          }
+        }
+      });
+
+      res.json({ allNotes: allNotesArr, pinnedNotes: pinnedNotesArr, otherNotes: otherNotesArr, discardNotes: discardNotesArr, folders : folderArr});
     })
     .catch(err => console.log(err));
 });
 
-app.post('/blogs', (req, res) => {
-  const note = new Note(req.body);
+// Return folders
+app.get('/folders', async (req, res) => {
+    const folders = await Folder.find().sort({ createdAt: -1 }).exec();
+    const notes = await Note.find().sort({ createdAt: -1 }).exec();
 
-  note.save()
+    const foldersWithNotes = {};
+
+    folders.forEach(folder => {
+      foldersWithNotes[folder.folder] = [];
+    });
+
+    // Populate the notes into their respective folder arrays
+    notes.forEach(note => {
+      if (foldersWithNotes.hasOwnProperty(note.folder) && note.discard == false) {
+        foldersWithNotes[note.folder].push(note);
+      }
+    });
+
+    res.json(foldersWithNotes);
+});
+
+// Create note
+app.post('/createNote', (req, res) => {
+  const note = new Note(req.title, req.body, req.folder, req.color, req.pinned, req.discard);
+
+  note.save().catch(err => console.log(err));
+});
+
+// Discard note
+app.patch('/note/:id/discard', async (req,res) => {
+  const noteId = req.params.id;
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { discard: true },
+    { new: true }
+  );
+  res.redirect('/notes');
+});
+
+// Recover note
+app.patch('/note/:id/recover', async (req,res) => {
+  const noteId = req.params.id;
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { discard: false },
+    { new: true }
+  );
+  res.redirect('/notes');
+});
+
+// Pin note
+app.patch('/note/:id/pin', async (req,res) => {
+  const noteId = req.params.id;
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { pinned: true },
+    { new: true }
+  );
+  res.redirect('/notes');
+});
+
+// Unpin note
+app.patch('/note/:id/unPin', async (req,res) => {
+  const noteId = req.params.id;
+
+  const updatedNote = await Note.findByIdAndUpdate(
+    noteId,
+    { pinned: false },
+    { new: true }
+  );
+  res.redirect('/notes');
+});
+
+//Delete note completely
+app.delete('/note/:id/delete', (req, res) => {
+  const noteId = req.params.id;
+
+  Note.findByIdAndDelete(id)
     .then(() => {
-      res.redirect('/blogs');
+      res.json({ redirect: '/notes' });
     })
     .catch(err => console.log(err));
 });
+
+//Delete folder
+app.delete('/folder/:id/delete', (req, res) => {
+  const folderId = req.params.id;
+
+  Folder.findByIdAndDelete(folderId)
+    .then(() => {
+      res.json({ redirect: '/folders' });
+    })
+    .catch(err => console.log(err));
+});
+
+
 
 app.get('/blogs/:id', (req, res) => {
   const id = req.params.id;
@@ -80,16 +182,6 @@ app.get('/blogs/:id', (req, res) => {
   Note.findById(id)
     .then(result => {
       res.render('details', { notes: result, title: 'Note Details' });
-    })
-    .catch(err => console.log(err));
-});
-
-app.delete('/blogs/:id', (req, res) => {
-  const id = req.params.id;
-
-  Note.findByIdAndDelete(id)
-    .then(() => {
-      res.json({ redirect: '/blogs' });
     })
     .catch(err => console.log(err));
 });
